b4b7c2748af8fbb66e179435dae73abb173d3c64
Marcelo Regueira <mregueira@itba.edu.ar>
2018-01-04 00:40:12 -0300

    Fix Player

--- TPF_Stratego/Player.h
+++ TPF_Stratego/Player.h
@@ -22,16 +22,16 @@ enum BasicStates{LOCAL_MOVE = 1, ENEMY_MOVE, WAIT_FOR_RANGE};
 class Player
 {
 public:
-	Player(PlayerType color);
+	Player(PlayerType color); /// Recibe color asignado y define quien comienza
 	MoveResult move_local_token(PosType src_pos, PosType dst_pos); /// Retorna MoveTypes
 	void process_attack(PosType src_pos, PosType dst_pos, RangeType attack_token_range); /// Recibe el rango atacante/atacado y resuelve											
 	MoveResult move_enemy_token(PosType src_pos, PosType dst_pos); /// Retorna MoveTypes		
-	State get_game_state();
+	State get_game_state(); /// Retorna estado actual segun BasicStates
 	~Player();
-	GameBoard local_board;
+	GameBoard local_board; 
 private:
 	PlayerType player;
-	State game_state;
-	vector <RangeType> tokens_lost;
+	State game_state; /// Se modifica automaticamente por {move_local_token - process_attack - move_enemy_token}
+	vector <RangeType> tokens_lost; /// Inicia vacio y almacena las fichas perdidas automaticamente
 };
 

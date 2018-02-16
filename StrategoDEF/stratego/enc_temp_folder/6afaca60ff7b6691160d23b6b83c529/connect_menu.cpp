dd1cc6d990cd03c0326745ce603c4d083436d4f7
newtonis <newtonis.penguin@gmail.com>
2018-02-16 03:25:16 -0300

    hello

--- StrategoDEF/stratego/enc_temp_folder/6afaca60ff7b6691160d23b6b83c529/connect_menu.cpp
+++ StrategoDEF/stratego/enc_temp_folder/6afaca60ff7b6691160d23b6b83c529/connect_menu.cpp
@@ -113,12 +113,12 @@ void ConnectMenu::startToConnect() {
 
 	setInfoObjects((Widget*)text,(Widget*)animation);
 
-	//callIn(1000, [](Sysgame *sys) {
+	callIn(1000, [](Sysgame *sys) {
 	
-		//ConnectMenu *myself = (ConnectMenu*)sys->getController();
-		//if (myself->getStatus() == "trying_to_connect")
-		//	myself->setConnectionFailed();
-	//});
+		ConnectMenu *myself = (ConnectMenu*)sys->getController();
+		if (myself->getStatus() == "trying_to_connect")
+			myself->setConnectionFailed();
+	});
 }
 void ConnectMenu::createConnectButton() {
 
